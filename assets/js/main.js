97c24685d7fca08355cc2a05cc9dfcfe67b1e2d5
Savas Tireng <savas.tireng@muuuh.de>
2021-11-01 11:00:54 +0100

    optimized css

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const inputField = document.querySelector('.inputfield')
   const inputButtons = document.querySelectorAll('[data-type="input"]')
+  const buttonClear = document.getElementById('clear')
+
+  buttonClear.addEventListener('click', () => inputField.innerHTML = '')
   
   inputButtons.forEach(inputButton => {
     inputButton.addEventListener('click', () => inputField.innerHTML += inputButton.innerHTML)
